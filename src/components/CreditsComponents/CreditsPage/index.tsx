396a5eca0fed035826d8f15261fd4406e06bbc07
Nameless <pintokevin2002@hotmail.com>
2026-02-25 16:13:32 +0100

    Add Index component for displaying credits and navigation

---
+++ src/components/CreditsComponents/CreditsPage/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import type {Page, VNStory} from "../../../interfaces/interfaces.ts";
+import PrimaryButton from "../../PrimaryButton";
+
+const Index: React.FC<{ script: VNStory, handleChangeRoom: (page: Page) => void }> = ({script, handleChangeRoom}) => {
+    const handleBackToTitle = () => handleChangeRoom("title");
+
+    return (
+        <div>
+            <h2>{script.settings.creditsPage.title}</h2>
+            <PrimaryButton
+                text="Back to Title"
+                onClick={handleBackToTitle}
+            />
+        </div>
+    );
+};
+
+export default Index;
