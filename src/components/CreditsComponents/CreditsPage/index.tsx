122e5a579551f75721ff6b0bf6bd9c13f54076ba
Nameless <pintokevin2002@hotmail.com>
2026-02-26 16:11:56 +0100

    Refactor CreditsPage component to use CreditsPage type and improve access to credits data

--- src/components/CreditsComponents/CreditsPage/index.tsx
+++ src/components/CreditsComponents/CreditsPage/index.tsx
@@ -1,20 +1,21 @@
 import * as React from "react";
-import type {CreditGroupType, Page, VNStory} from "../../../interfaces/interfaces.ts";
+import type {CreditGroupType, CreditsPage, Page, VNStory} from "../../../interfaces/interfaces.ts";
 import PrimaryButton from "../../PrimaryButton";
 import CreditGroup from "../CreditGroup";
 
 const CreditsPage: React.FC<{ script: VNStory, handleChangeRoom: (page: Page) => void }> = ({script, handleChangeRoom}) => {
-    const handleBackToTitle = () => handleChangeRoom("title");
+    const creditsPage: CreditsPage = script.settings.creditsPage;
+    const handleBackToTitle = (): void => handleChangeRoom("title");
 
     return (
         <div className="h-100 centered column">
-            <h2>{script.settings.creditsPage.title}</h2>
+            <h2>{creditsPage.title}</h2>
             <PrimaryButton
                 text="Back to Title"
                 onClick={handleBackToTitle}
             />
 
-            {script.settings.creditsPage.creditGroups.map((cg: CreditGroupType, i: number) => (
+            {creditsPage.creditGroups.map((cg: CreditGroupType, i: number) => (
                 <CreditGroup creditGroup={cg} key={i} />
             ))}
         </div>
