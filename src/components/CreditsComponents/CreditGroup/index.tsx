637d489fb1e499bb395d292f162f0d3c7dbd471a
Nameless <pintokevin2002@hotmail.com>
2026-02-26 16:09:45 +0100

    Refactor CreditGroup component to use TypeScript and improve rendering of credit elements

--- src/components/CreditsComponents/CreditGroup/index.tsx
+++ src/components/CreditsComponents/CreditGroup/index.tsx
@@ -1,7 +1,15 @@
-const CreditGroup = () => {
-    return (
-        <div>
+import * as React from "react";
+import type {Credit, CreditGroupType} from "../../../interfaces/interfaces.ts";
+import CreditEl from "../CreditEl";
 
+const CreditGroup: React.FC<{ creditGroup: CreditGroupType }> = ({creditGroup}) => {
+    return (
+        <div className="credit-group centered column">
+            <h2>{creditGroup.groupName}</h2>
+
+            {creditGroup.credits.map((credit: Credit, i: number) => (
+                <CreditEl credit={credit} key={i} />
+            ))}
         </div>
     );
 };
