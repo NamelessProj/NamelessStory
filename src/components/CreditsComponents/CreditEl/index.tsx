9c737e18f6839f8d0f6f08b10b8d7c452e4d3707
Nameless <pintokevin2002@hotmail.com>
2026-02-26 16:09:40 +0100

    Refactor CreditEl component to use TypeScript and improve structure

--- src/components/CreditsComponents/CreditEl/index.tsx
+++ src/components/CreditsComponents/CreditEl/index.tsx
@@ -1,7 +1,11 @@
-const CreditEl = () => {
-    return (
-        <div>
+import * as React from "react";
+import type {Credit} from "../../../interfaces/interfaces.ts";
 
+const CreditEl: React.FC<{ credit: Credit }> = ({credit}) => {
+    return (
+        <div className="credit-el centered column">
+            <h3>{credit.name}</h3>
+            <p>{credit.role}</p>
         </div>
     );
 };
