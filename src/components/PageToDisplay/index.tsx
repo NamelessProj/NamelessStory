7e26e7e8298ad5983f2ebc55b8687b2e514865f8
Nameless <pintokevin2002@hotmail.com>
2026-02-26 21:35:48 +0100

    Add PageToDisplay component for dynamic page rendering

---
+++ src/components/PageToDisplay/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import type {Page, VNStory} from "../../interfaces/interfaces.ts";
+import TitleScreen from "../TitleScreen";
+import CreditsPage from "../CreditsComponents/CreditsPage";
+
+const PageToDisplay: React.FC<{ page: Page, script: VNStory, handleChangePage: () => void }> = ({page, script, handleChangePage}) => {
+    switch (page) {
+        case "title":
+            return <TitleScreen script={script} handleStart={() => handleChangePage("game")} handleCredits={() => handleChangePage("credits")} />
+        case "credits":
+            return <CreditsPage script={script} handleChangeRoom={handleChangePage} />
+        case "game":
+            return <div>Game</div>
+        default:
+            return null;
+    }
+}
+
+export default PageToDisplay;
