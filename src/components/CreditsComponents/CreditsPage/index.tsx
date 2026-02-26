fed7cd100f2565283885ce1c393c939d3a27cf15
Nameless <pintokevin2002@hotmail.com>
2026-02-26 21:01:18 +0100

    Add useEffect to handle automatic navigation after credits scroll

--- src/components/CreditsComponents/CreditsPage/index.tsx
+++ src/components/CreditsComponents/CreditsPage/index.tsx
@@ -2,14 +2,20 @@ import * as React from "react";
 import type {CreditGroupType, CreditsPage, Page, VNStory} from "../../../interfaces/interfaces.ts";
 import CreditGroup from "../CreditGroup";
 import './style.css';
-import type {CSSProperties} from "react";
+import {type CSSProperties, useEffect} from "react";
 
 const CreditsPage: React.FC<{ script: VNStory, handleChangeRoom: (page: Page) => void }> = ({script, handleChangeRoom}) => {
     const creditsPage: CreditsPage = script.settings.creditsPage;
-    const handleBackToTitle = (): void => handleChangeRoom("title");
 
     const scrollDurationInSeconds: number = creditsPage.scrollDurationInSeconds || 30;
     const style: CSSProperties = {"--credits-scroll-time": `${scrollDurationInSeconds}s`} as CSSProperties;
+
+    useEffect((): () => void => {
+        const timeInMs: number = scrollDurationInSeconds * 1000 + 2000; // Add 2 seconds to ensure the scroll finishes before changing room
+        const timer: number = setTimeout((): void => handleChangeRoom("title"), timeInMs);
+
+        return (): void => clearTimeout(timer);
+    }, [handleChangeRoom, scrollDurationInSeconds]);
 
     return (
         <div className="credits-page h-100 centered column" style={style}>
