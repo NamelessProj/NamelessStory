dbcaa2ebe49da260dcbaf4fb0b5492a12ef7c44d
Nameless <pintokevin2002@hotmail.com>
2026-02-27 19:05:39 +0100

    Add optional id prop to BackgroundImage component for enhanced flexibility

--- src/components/BackgroundImage/index.tsx
+++ src/components/BackgroundImage/index.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import './style.css';
 
-const BackgroundImage: React.FC<{ fileName: string }> = ({fileName}) => {
+const BackgroundImage: React.FC<{ fileName: string, id?: string }> = ({fileName, id}) => {
     return (
         <picture className="bg-img">
             <img
                 src={`../assets/${fileName}`}
                 alt="Credits Background"
+                id={id}
             />
         </picture>
     );
