7ef3854793c45edcdd3f2528d5c5c540c6b2a470
Nameless <pintokevin2002@hotmail.com>
2026-03-18 16:15:06 +0100

    Pass state prop to PageToDisplay component for enhanced game functionality

--- src/components/PageToDisplay/index.tsx
+++ src/components/PageToDisplay/index.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
-import type {Page, VNStory} from "../../interfaces/interfaces.ts";
+import type {Page, State, VNStory} from "../../interfaces/interfaces.ts";
 import TitleScreen from "../TitleScreen";
 import CreditsPage from "../CreditsComponents/CreditsPage";
+import VisualNovel from "../VisualNovelComponents/VisualNovel";
 
-const PageToDisplay: React.FC<{ page: Page, script: VNStory, handleChangePage: (newPage: Page) => void }> = ({page, script, handleChangePage}) => {
+const PageToDisplay: React.FC<{ page: Page, script: VNStory, state: State, handleChangePage: (newPage: Page) => void }> = ({page, script, state, handleChangePage}) => {
     switch (page) {
         case "title":
             return <TitleScreen script={script} handleStart={() => handleChangePage("game")} handleCredits={() => handleChangePage("credits")} />
         case "credits":
             return <CreditsPage script={script} handleChangeRoom={handleChangePage} />
         case "game":
-            return <div>Game</div>
+            return <VisualNovel script={script} state={state} />
         default:
             return null;
     }
