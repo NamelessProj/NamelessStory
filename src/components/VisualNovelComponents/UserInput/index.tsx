607378ccb725f815b6f2f60517e595c49b155a3a
Nameless <pintokevin2002@hotmail.com>
2026-03-26 16:38:18 +0100

    Add UserInput component for handling user input with type safety

---
+++ src/components/VisualNovelComponents/UserInput/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import type {VariableType} from "../../../interfaces/interfaces.ts";
+
+const UserInput: React.FC<{ userInput: VariableType, input: string, setInput: (value: string, variableName: string, color?: string) => void }> = ({userInput, input, setInput}) => {
+    return (
+        <div>
+            <input
+                value={input}
+                onChange={(e): void => setInput(e.target.value, userInput.value, userInput.color ?? undefined)}
+            />
+        </div>
+    );
+};
+
+export default UserInput;
