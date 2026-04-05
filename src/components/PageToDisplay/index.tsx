826fa9d398bf64b7eebac256092c0e42672b7eda
Nameless <pintokevin2002@hotmail.com>
2026-04-05 17:01:26 +0200

    Refactor PageToDisplay component for improved prop handling and state management

--- src/components/PageToDisplay/index.tsx
+++ src/components/PageToDisplay/index.tsx
@@ -4,17 +4,25 @@ import TitleScreen from "../TitleScreen";
 import CreditsPage from "../CreditsComponents/CreditsPage";
 import VisualNovel from "../VisualNovelComponents/VisualNovel";
 
-const PageToDisplay: React.FC<{ page: Page, script: VNStory, state: State, handleChangePage: (newPage: Page) => void }> = ({page, script, state, handleChangePage}) => {
+interface PageToDisplayProps {
+    page: Page;
+    script: VNStory;
+    state: State;
+    setState: (newState: State) => void;
+    handleChangePage: (newPage: Page) => void;
+}
+
+const PageToDisplay: React.FC<PageToDisplayProps> = ({page, script, state, setState, handleChangePage}) => {
     switch (page) {
         case "title":
             return <TitleScreen script={script} handleStart={() => handleChangePage("game")} handleCredits={() => handleChangePage("credits")} />
         case "credits":
             return <CreditsPage script={script} handleChangeRoom={handleChangePage} />
         case "game":
-            return <VisualNovel script={script} state={state} />
+            return <VisualNovel script={script} state={state} setState={setState} />
         default:
             return null;
     }
-}
+};
 
 export default PageToDisplay;
