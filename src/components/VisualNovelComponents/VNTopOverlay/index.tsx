c74bbe0c6d21880990c141d541f41e557c86f0b7
Nameless <pintokevin2002@hotmail.com>
2026-04-08 21:54:21 +0200

    Add music mute toggle functionality to VNTopOverlay component

--- src/components/VisualNovelComponents/VNTopOverlay/index.tsx
+++ src/components/VisualNovelComponents/VNTopOverlay/index.tsx
@@ -1,15 +1,36 @@
 import * as React from "react";
 
 import "./style.css";
+import type {State} from "../../../interfaces/interfaces.ts";
 
 interface VNOverlayProps {
     isOverlayHidden: boolean;
+    state: State;
+    setState: (state: State) => void;
 }
 
-const VNTopOverlay: React.FC<VNOverlayProps> = ({isOverlayHidden}) => {
+const VNTopOverlay: React.FC<VNOverlayProps> = ({isOverlayHidden, state, setState}) => {
+    // Toggle music mute
+    const toggleMute = (): void => {
+        const newMutedState = !state.isMusicMuted;
+        const newVolume = newMutedState ? 0 : state.musicVolume;
+
+        if (state.currentMusic) {
+            state.currentMusic.volume = newMutedState ? 0 : state.musicVolume;
+        }
+
+        setState({
+            ...state,
+            isMusicMuted: newMutedState,
+            musicVolume: newVolume
+        });
+    };
+
     return (
         <div className={`vn-overlay vn-overlay-top ${isOverlayHidden ? "hidden" : "show"}`}>
-
+            <button className="overlay-button" onClick={toggleMute}>
+                {state.isMusicMuted ? "Unmute Music" : "Mute Music"}
+            </button>
         </div>
     );
 };
