49470e4e4872f4831f3601acb4f367409a2a247f
Nameless <pintokevin2002@hotmail.com>
2026-04-13 14:14:05 +0200

    Add advance threshold constant and fix newline in VARIABLE_REGEX_SINGLE

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,3 +9,6 @@ export const PAUSE_SYMBOL: string = "\\";
 
 export const VARIABLE_REGEX: RegExp = /{{([vcC]!)?([a-zA-Z0-9]+)}}/g;
 export const VARIABLE_REGEX_SINGLE: RegExp = /{{([vcC]!)?([a-zA-Z0-9]+)}}/;
+
+// Minimum ms to wait after typing completes before the player can advance
+export const ADVANCE_THRESHOLD_MS: number = 150;
