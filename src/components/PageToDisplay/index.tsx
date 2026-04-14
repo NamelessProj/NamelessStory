7f769de283fdc4f0142e436d12eaff6e0947239a
Nameless <pintokevin2002@hotmail.com>
2026-04-14 19:43:50 +0200

    Add optional handleContinue prop to PageToDisplay component

--- src/components/PageToDisplay/index.tsx
+++ src/components/PageToDisplay/index.tsx
@@ -7,12 +7,13 @@ import VisualNovel from "../VisualNovelComponents/VisualNovel";
 interface PageToDisplayProps {
     page: Page;
     handleChangePage: (newPage: Page) => void;
+    handleContinue?: () => void;
 }
 
-const PageToDisplay: React.FC<PageToDisplayProps> = ({page, handleChangePage}) => {
+const PageToDisplay: React.FC<PageToDisplayProps> = ({page, handleChangePage, handleContinue}) => {
     switch (page) {
         case "title":
-            return <TitleScreen handleStart={() => handleChangePage("game")} handleCredits={() => handleChangePage("credits")} />
+            return <TitleScreen handleStart={() => handleChangePage("game")} handleCredits={() => handleChangePage("credits")} handleContinue={handleContinue} />
         case "credits":
             return <CreditsPage handleChangeRoom={handleChangePage} />
         case "game":
