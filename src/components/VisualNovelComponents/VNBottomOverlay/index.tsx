562656de2b1a8c0f46e02798c41bb461cadbd4aa
Nameless <pintokevin2002@hotmail.com>
2026-04-15 21:05:55 +0200

    Rename saveFunc to exportSaveFunc in VNBottomOverlay component for clarity

--- src/components/VisualNovelComponents/VNBottomOverlay/index.tsx
+++ src/components/VisualNovelComponents/VNBottomOverlay/index.tsx
@@ -3,21 +3,21 @@ import type {Page} from "../../../interfaces/interfaces.ts";
 import './style.css';
 
 interface VNOverlayProps {
-    saveFunc: () => void;
+    exportSaveFunc: () => void;
     setPage: (page: Page) => void;
     isOverlayHidden: boolean;
     setIsOverlayHidden: (hidden: boolean) => void;
 }
 
-const VNBottomOverlay = ({saveFunc, setPage, isOverlayHidden, setIsOverlayHidden}: VNOverlayProps) => {
+const VNBottomOverlay = ({exportSaveFunc, setPage, isOverlayHidden, setIsOverlayHidden}: VNOverlayProps) => {
     return (
         <div className={`vn-overlay vn-overlay-bottom ${isOverlayHidden ? "hidden" : "show"}`}>
             <div className="buttons-wrapper">
                 <button className="overlay-button" onClick={(): void => setPage("title")}>
                     Return Home
                 </button>
-                <button className="overlay-button" onClick={saveFunc}>
-                    Save
+                <button className="overlay-button" onClick={exportSaveFunc}>
+                    Export Save
                 </button>
                 <button className="overlay-button" onClick={(): void => setIsOverlayHidden(true)}>
                     Hide Overlay
