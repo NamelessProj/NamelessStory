6d2c71d1923a0840a5ba0aac92b01890d2ae270f
Pinto <pintokevin2002@hotmail.com>
2026-04-17 10:07:14 +0200

    Deletion of the unused vn.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 import './style.css'
-import './vn.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
