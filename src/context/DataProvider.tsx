d86318b7118de8cd504417cb9fcda50a4ff68ba2
Nameless <pintokevin2002@hotmail.com>
2026-04-20 21:07:07 +0200

    Add TypewriterContext provider to DataProvider for improved state management

--- src/context/DataProvider.tsx
+++ src/context/DataProvider.tsx
@@ -1,14 +1,18 @@
 import { type ReactNode } from "react";
 import DataContext, { type DataContextValue } from "./DataContext.ts";
+import TypewriterContext, { type TypewriterContextValue } from "./TypewriterContext.ts";
 
 interface DataProviderProps {
     children: ReactNode;
     value: DataContextValue;
+    typewriterValue: TypewriterContextValue;
 }
 
-const DataProvider = ({ children, value }: DataProviderProps) => (
+const DataProvider = ({ children, value, typewriterValue }: DataProviderProps) => (
     <DataContext.Provider value={value}>
-        {children}
+        <TypewriterContext.Provider value={typewriterValue}>
+            {children}
+        </TypewriterContext.Provider>
     </DataContext.Provider>
 );
 
